03380f0558d97c8fa8aad143f7bc3633fab05a9e
ifranco21 <ifranco7302@gmail.com>
2026-04-26 00:55:19 -0400

    fix duplicate home route

--- webapp/app/routes.ts
+++ webapp/app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("home", "routes/home.tsx"),
+  //route("home", "routes/home.tsx"), removed due to duplication error
   route("inventory", "routes/inventory.tsx"),
   route("itemcreate", "routes/itemcreate.tsx"),
 ] satisfies RouteConfig;
